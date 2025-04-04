77171bb5d788b2201dd5bb872409177ec56bba11
Siva <siva9kolli@gmail.com>
2025-04-04 10:18:33 +0530

    selenium introduction 2

--- src/main/java/com/hyperGrid/aboutSelenium/LaunchBrowser.java
+++ src/main/java/com/hyperGrid/aboutSelenium/LaunchBrowser.java
@@ -1,12 +1,23 @@
 package com.hyperGrid.aboutSelenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LaunchBrowser {
 
     public static void main(String[] args) {
-        WebDriver webDriver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Siva\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //chromeOptions.setBrowserVersion("126");
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
+        webDriver.manage().window().maximize();
         webDriver.navigate().to("https://www.saucedemo.com/");
+        //webDriver.get("https://google.com/");
+
+        webDriver.findElement(By.name("user-name")).sendKeys("standard_user");
+        webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
+        webDriver.findElement(By.id("login-button")).click();
     }
 }
